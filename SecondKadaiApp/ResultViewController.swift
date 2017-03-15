f9dd7f13c8ca65d62612a1c1b11dff93c8f19d8d
MAC USER <macuser@MAC-no-MacBook-Air-2.local>
2017-03-15 19:04:38 +0900

    FirstVersion

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by MAC USER on 2017/03/14.
+//  Copyright © 2017年 MAC USER. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    var personname = ""
+    
+    @IBOutlet weak var Hyouji: UILabel!
+    @IBOutlet weak var modoru: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Hyouji.text = "こんにちは、\(personname)さん"
+        // Do any additional setup after loading the view.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Setumei: UILabel!
+    @IBOutlet weak var Nyuuryoku: UITextField!
+    @IBOutlet weak var button: UIButton!
+    func tappedButton (sender: UIButton){
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //定義したオブジェクトについての記述はここに書く
+        Setumei.text = "名前を入力してください"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
+        
+        let sVC = segue.destination as! ResultViewController
+        sVC.personname = Nyuuryoku.text!
+    }
+    
 }
 
